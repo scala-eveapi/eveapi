1d76f859c20d17e9c8a9fee8b0f4512dd7ba1bfd
Andi Miller <andi@andimiller.net>
2016-06-10 20:58:53 +0100

    add in other CREST types

---
+++ data/src/main/scala/eveapi/data/crest/character/location/Types.scala
@@ -0,0 +1,22 @@
+package eveapi.data.crest.character.location
+
+
+case class SolarSystem(
+                        id_str: String,
+                        href: String,
+                        id: Long,
+                        name: String
+                      )
+
+case class Location(
+                     solarSystem: Option[SolarSystem],
+                     station: Option[Station]
+                   )
+
+case class Station(
+                    id_str: String,
+                    href: String,
+                    id: Long,
+                    name: String
+                  )
+
---
+++ data/src/main/scala/eveapi/data/crest/solarsystem/Types.scala
@@ -0,0 +1,30 @@
+package eveapi.data.crest.solarsystem
+
+case class Href(
+                 href: String
+               )
+
+case class Position(
+                     y: Double,
+                     x: Double,
+                     z: Double
+                   )
+
+case class Constellation(
+                          href: String,
+                          id: Long,
+                          id_str: String
+                        )
+
+case class SolarSystem(
+                        stats: Href,
+                        name: String,
+                        securityStatus: Double,
+                        securityClass: String,
+                        href: String,
+                        id_str: String,
+                        planets: List[Href],
+                        position: Position,
+                        constellation: Constellation,
+                        id: Long
+                      )
