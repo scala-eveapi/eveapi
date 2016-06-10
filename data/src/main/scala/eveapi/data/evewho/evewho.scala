f5b9aaa4052e88d5d5ee3171d9e2ad4474e17ff4
Andi Miller <andi@andimiller.net>
2016-06-10 20:11:18 +0100

    check in evewho and zkillboard types

---
+++ data/src/main/scala/eveapi/data/evewho/evewho.scala
@@ -0,0 +1,52 @@
+package eveapi.data.evewho
+
+case class CharacterInfo(
+                          character_id: String,
+                          corporation_id: String,
+                          alliance_id: String,
+                          faction_id: String,
+                          name: String,
+                          sec_status: String
+                        )
+
+case class CharacterHistory(
+                             corporation_id: String,
+                             start_date: String,
+                             end_date: String
+                           )
+
+case class CharacterData(
+                          info: CharacterInfo,
+                          history: List[CharacterHistory]
+                        )
+
+
+case class CorpInfo(
+                     corporation_id: String,
+                     name: String,
+                     memberCount: String
+                   )
+
+case class Character(
+                      character_id: Long,
+                      corporation_id: Long,
+                      alliance_id: Long,
+                      name: String
+                    )
+
+case class CorpData(
+                     info: CorpInfo,
+                     characters: List[Character]
+                   )
+
+case class AllianceInfo(
+                         alliance_id: String,
+                         name: String,
+                         memberCount: String
+                       )
+
+case class AllianceData(
+                         info: AllianceInfo,
+                         characters: List[Character]
+                       )
+
---
+++ data/src/main/scala/eveapi/data/zkillboard/zkillboard.scala
@@ -0,0 +1,102 @@
+package eveapi.data.zkillboard
+
+
+case class Victim(
+                   x: Double,
+                   y: Double,
+                   z: Double,
+                   shipTypeID: Long,
+                   characterID: Long,
+                   characterName: String,
+                   corporationID: Long,
+                   corporationName: String,
+                   allianceID: Long,
+                   allianceName: String,
+                   factionID: Long,
+                   factionName: String,
+                   damageTaken: Long
+                 )
+
+case class Attackers(
+                      characterID: Long,
+                      characterName: String,
+                      corporationID: Long,
+                      corporationName: String,
+                      allianceID: Long,
+                      allianceName: String,
+                      factionID: Long,
+                      factionName: String,
+                      securityStatus: Long,
+                      damageDone: Double,
+                      finalBlow: Long,
+                      weaponTypeID: Long,
+                      shipTypeID: Long
+                    )
+
+case class Items(
+                  typeID: Long,
+                  flag: Long,
+                  qtyDropped: Long,
+                  qtyDestroyed: Long,
+                  singleton: Long
+                )
+
+case class Killmail(
+                     killID: Long,
+                     solarSystemID: Long,
+                     killTime: String,
+                     moonID: Long,
+                     victim: Victim,
+                     attackers: List[Attackers],
+                     items: List[Items]
+                   )
+
+case class SolarSystem(
+                        id_str: String,
+                        href: String,
+                        id: Long,
+                        name: String
+                      )
+
+case class Icon(
+                 href: String
+               )
+
+case class Href(
+                 id_str: String,
+                 href: String,
+                 id: Long,
+                 name: String,
+                 icon: Icon
+               )
+
+case class Position(
+                     y: Double,
+                     x: Double,
+                     z: Double
+                   )
+
+case class War(
+                href: String,
+                id: Long,
+                id_str: String
+              )
+
+case class Zkb(
+                locationID: Long,
+                hash: String,
+                totalValue: Double,
+                points: Long,
+                href: String
+              )
+
+case class Package(
+                    killID: Long,
+                    killmail: Killmail,
+                    zkb: Zkb
+                  )
+
+case class RedisQResponse(
+                           `package`: Option[Package]
+                         )
+
