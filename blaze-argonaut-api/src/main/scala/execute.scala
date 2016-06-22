dc51a67a459f219bfd2d1bff848ea468410724ee
Simon Hafner <hafnersimon@gmail.com>
2016-06-22 21:13:38 +0200

    File renames

--- blaze-argonaut-api/src/main/scala/execute.scala
+++ blaze-argonaut-api/src/main/scala/execute.scala
@@ -1,32 +1,29 @@
-package eveapi.execute
+package eveapi.blazeClient
+
 import scalaz._, Scalaz._
-import eveapi.data.crest._
-import argonaut._
+import scalaz.concurrent._
 import org.http4s._
+import org.http4s.argonaut._
 
-object Lift {
-  sealed abstract class Link[T] extends Serializable with Product {
-    def decoder: DecodeJson[T]
+import eveapi.execute._
+import eveapi.oauth._
+import eveapi.utils.TaskEffect._
+
+object Execute {
+  import OAuth2._
+  def OAuthInterpreter(oauth: OAuth2) = new (Lift.Link ~> Api) {
+    def apply[T](l: Lift.Link[T]) = l match {
+      case Lift.Get(link, dec) => oauth.fetch(Request(uri = link.href, method = Method.GET))(dec)
+      case Lift.Put(link, value, dec, enc) =>
+        innocentTask[EveApiS, Request](
+            Request(uri = link.href, method = Method.PUT).withBody(enc.encode(value)))
+          .flatMap(req => oauth.fetch(req)(dec))
+      case Lift.Post(link, value, dec, enc) =>
+        innocentTask[EveApiS, Request](
+            Request(uri = link.href, method = Method.POST).withBody(enc.encode(value)))
+          .flatMap(req => oauth.fetch(req)(dec))
+      case Lift.Delete(link, dec) =>
+        oauth.fetch(Request(uri = link.href, method = Method.DELETE))(dec)
+    }
   }
-  case class Get[T](link: GetLink[Uri, T], decoder: DecodeJson[T]) extends Link[T]
-  case class Put[T](
-    link: PutLink[Uri, T], value: T, decoder: DecodeJson[UpdateResponse], encoder: EncodeJson[T])
-      extends Link[UpdateResponse]
-  case class Post[T](link: PostLink[Uri, T],
-                     value: T,
-                     decoder: DecodeJson[CreationResponse],
-                     encoder: EncodeJson[T])
-      extends Link[CreationResponse]
-  case class Delete(link: DeleteLink[Uri], decoder: DecodeJson[DeletionResponse])
-      extends Link[DeletionResponse]
-
-  def get[T](l: GetLink[Uri, T])(implicit dec: DecodeJson[T]) = Free.liftF(Get(l, dec))
-  def put[T](l: PutLink[Uri, T], value: T)(
-    implicit dec: DecodeJson[UpdateResponse], enc: EncodeJson[T]) =
-    Free.liftF(Put(l, value, dec, enc))
-  def post[T](l: PostLink[Uri, T], value: T)(
-    implicit dec: DecodeJson[CreationResponse], enc: EncodeJson[T]) =
-    Free.liftF(Post(l, value, dec, enc))
-  def delete(l: DeleteLink[Uri])(implicit dec: DecodeJson[DeletionResponse]) =
-    Free.liftF(Delete(l, dec))
 }
