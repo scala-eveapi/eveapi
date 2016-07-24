cd21c6219c83b495cad1a6eddad9ab3c4d17faa4
Simon Hafner <hafnersimon@gmail.com>
2016-07-24 22:47:13 +0800

    Added a CodeJson for java.time.Instant

--- blaze-argonaut-api/src/main/scala/codecs.scala
+++ blaze-argonaut-api/src/main/scala/codecs.scala
@@ -1,10 +1,13 @@
 package eveapi.utils
 
 import argonaut._
+import java.time.format.DateTimeParseException
 import org.http4s.Uri
+import java.time.Instant
+import scalaz._, Scalaz._
 
 object Decoders {
-  implicit val urlcodec: CodecJson[Uri] = CodecJson(
+  implicit val urlCodec: CodecJson[Uri] = CodecJson(
       (uri: Uri) => Json.jString(uri.toString),
       c =>
         c.as[String]
@@ -12,6 +15,17 @@ object Decoders {
                 Uri
                   .fromString(str)
                   .fold(err => DecodeResult.fail(err.toString, c.history),
-                        uri => DecodeResult.ok(uri)))
+                        DecodeResult.ok))
+  )
+  implicit val instantCodec: CodecJson[Instant] = CodecJson(
+    (instant: Instant) => Json.jString(instant.toString),
+    c => c.as[String].flatMap(str =>
+      \/.fromTryCatchNonFatal(Instant.parse(str)).fold(err => err match {
+        case e: DateTimeParseException => DecodeResult.fail(e.toString, c.history)
+        case e => throw e
+      },
+        DecodeResult.ok
+      )
+    )
   )
 }
