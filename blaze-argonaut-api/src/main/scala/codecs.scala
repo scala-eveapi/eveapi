235bbe89a8bec0af6139dc3fa754128b47ffc0ee
Simon Hafner <hafnersimon@gmail.com>
2016-08-14 20:25:45 +0800

    CCP sends LocalDateTime

--- blaze-argonaut-api/src/main/scala/codecs.scala
+++ blaze-argonaut-api/src/main/scala/codecs.scala
@@ -3,7 +3,7 @@ package eveapi.utils
 import argonaut._
 import java.time.format.DateTimeParseException
 import org.http4s.Uri
-import java.time.Instant
+import java.time._
 import scalaz._, Scalaz._
 
 object Decoders {
@@ -14,18 +14,22 @@ object Decoders {
           .flatMap(str =>
                 Uri
                   .fromString(str)
-                  .fold(err => DecodeResult.fail(err.toString, c.history),
-                        DecodeResult.ok))
+                  .fold(err => DecodeResult.fail(err.toString, c.history), DecodeResult.ok))
   )
   implicit val instantCodec: CodecJson[Instant] = CodecJson(
-    (instant: Instant) => Json.jString(instant.toString),
-    c => c.as[String].flatMap(str =>
-      \/.fromTryCatchNonFatal(Instant.parse(str)).fold(err => err match {
-        case e: DateTimeParseException => DecodeResult.fail(e.toString, c.history)
-        case e => throw e
-      },
-        DecodeResult.ok
-      )
-    )
+      (instant: Instant) => Json.jString(instant.toString),
+      c =>
+        c.as[String]
+          .flatMap(
+              str =>
+                \/.fromTryCatchNonFatal(Instant.parse(str))
+                  .orElse(
+                      \/.fromTryCatchNonFatal(LocalDateTime.parse(str).toInstant(ZoneOffset.UTC)))
+                  .fold(err =>
+                          err match {
+                        case e: DateTimeParseException => DecodeResult.fail(e.toString, c.history)
+                        case e => throw e
+                    },
+                        DecodeResult.ok))
   )
 }
