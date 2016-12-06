1499b47dab36fb91247edc1dbc6ed25306d224bb
Andi Miller <andi@andimiller.net>
2016-12-06 14:49:59 +0000

    load bintray credentials before we compile

--- ci.sh
+++ ci.sh
@@ -1,9 +1,9 @@
-# compile
-sbt compile
-
 # load bintray credentials
 mkdir -p ~/.bintray
 echo $BINTRAY_CREDENTIALS | sed 's/>/\n/g' > ~/.bintray/.credentials
+
+# compile
+sbt compile
 
 # publish
 sbt publish
